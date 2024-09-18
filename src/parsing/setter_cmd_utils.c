b09f0d7aabbd68a02b9fbac32d9e11ea65e116ce
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-09-18 17:51:57 +0200

    Handling expand in heredoc

--- src/parsing/setter_cmd_utils.c
+++ src/parsing/setter_cmd_utils.c
@@ -6,68 +6,11 @@
 /*   By: rbouselh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 16:34:38 by rbouselh          #+#    #+#             */
-/*   Updated: 2024/09/13 19:40:34 by labdello         ###   ########.fr       */
+/*   Updated: 2024/09/18 17:49:05 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	rl_heredoc(char *eof, int tmp_fd)
-{
-	char	*line;
-
-	signal(SIGINT, &heredoc_sig);
-	while (1)
-	{
-		line = readline("> ");
-		if (!line || ft_strcmp(line, eof) == 0)
-		{
-			close(tmp_fd);
-			free(line);
-			exit(0);
-		}
-		ft_putendl_fd(line, tmp_fd);
-		free(line);
-	}
-}
-
-int	heredoc_fork(char *eof, int *status, t_env *env, int fd)
-{
-	int		pid;
-
-	pid = fork();
-	if (pid == -1)
-		return (0);
-	if (pid == 0)
-	{
-		close(env->fd_in);
-		close(env->fd_out);
-		rl_heredoc(eof, fd);
-	}
-	waitpid(pid, status, 0);
-	return (1);
-}
-
-int	here_doc(char *eof, t_env *env)
-{
-	int		fd;
-	int		status;
-
-	signal(SIGINT, SIG_IGN);
-	if (access("/tmp/.heredoc", F_OK | X_OK) == -1)
-		unlink("/tmp/.heredoc");
-	fd = open("/tmp/.heredoc", O_RDWR | O_CREAT | O_TRUNC, 0777);
-	if (!heredoc_fork(eof, &status, env, fd))
-		return (close(fd), -1);
-	if (WIFEXITED(status))
-		g_signal_status = WEXITSTATUS(status);
-	signal(SIGINT, &readline_sig);
-	close(fd);
-	fd = open("/tmp/.heredoc", O_RDWR);
-	if (access("/tmp/.heredoc", F_OK | X_OK) == -1)
-		return (close(fd), -1);
-	return (fd);
-}
 
 int	set_file(t_file *current, t_env *env)
 {
